891127df29e4ef81f5abcd8f607919ed51d8cf00
vm7608 <102200270@sv1.dut.udn.vn>
2024-05-12 11:48:51 +0700

    Update script

--- scripts/download_test.sh
+++ scripts/download_test.sh
@@ -1,2 +1,6 @@
-folder_id=1fcH0Km2MvhHNOCJSq4sF8eomjo5bCaBX
-gdown --folder https://drive.google.com/drive/folders/$folder_id
+# id=1fcH0Km2MvhHNOCJSq4sF8eomjo5bCaBX
+# gdown --folder https://drive.google.com/drive/folders/$id
+
+id=1MMo-WRzG1z3BvuQvHrAOoTMrOGWyd2f-
+gdown https://drive.google.com/uc?id=$id
+unzip -q test.zip
